12d67f6fc68f4cc852a36fb27596cd7cfd7c6c6b
happy_sadan <tristancottam.1@gmail.com>
2020-10-15 13:40:22 +0200

    Attempt 2
    Make ft_substr() return an empty string when the start index is bigger
    than the string length.

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: trcottam <trcottam@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 19:56:11 by trcottam          #+#    #+#             */
-/*   Updated: 2020/02/13 13:09:33 by trcottam         ###   ########.fr       */
+/*   Updated: 2020/10/15 13:39:48 by trcottam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*substr;
 	size_t	i;
 
-	if (!s ||
-			start > ft_strlen(s) ||
-			!(substr = malloc(sizeof(char) * (len + 1))))
+	if (!s || !(substr = malloc(sizeof(char) * (len + 1))))
 		return (NULL);
 	i = 0;
 	while (s[i] && i < len)
