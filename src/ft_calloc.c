565702000b9a6eb69ec4bac49ce632b5f779394c
happy_sadan <tristancottam.1@gmail.com>
2021-01-07 01:42:40 +0100

    fix(func): Fix ft_calloc()

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: trcottam <trcottam@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 19:31:06 by trcottam          #+#    #+#             */
-/*   Updated: 2021/01/06 13:59:58 by trcottam         ###   ########.fr       */
+/*   Updated: 2021/01/06 16:47:35 by trcottam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ void	*ft_calloc(size_t count, size_t size)
 
 	if (!(p = malloc(size * count)))
 		return (NULL);
-	ft_memset(p, count, size);
+	ft_memset(p, '\0', size * count);
 	return (p);
 }
